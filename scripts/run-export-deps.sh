79b51798d219c8059f7fce490c44ee0a5236f490
hermann-web <hermannagossou6@gmail.com>
2024-04-06 20:39:52 +0000

    setup for readthedocs deployment

--- scripts/run-export-deps.sh
+++ scripts/run-export-deps.sh
@@ -1,4 +1,3 @@
 
-poetry export -f requirements.txt --output ./docs/requirements.txt --without-hashes --only visu --only pandas --only sklearn
+poetry export -f requirements.txt --output ./docs/requirements.txt --without-hashes --with visu --with pandas --with sklearn --with buildthedocs
 poetry export -f requirements.txt --output ./requirements.txt --without-hashes
-
